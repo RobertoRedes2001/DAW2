2814b36a09cacf1a4bd4e86ed18a80028ba3e0ac
daw2 <daw2@alumnado.flo>
2023-09-12 10:00:43 +0200

    primeros contenidos

---
+++ UD0/DOCKER/index.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+            <?php
+                $nombre = "Hola";
+                $ini = $nombre[1];
+                //Muestra un "Hola" por pantalla
+                print ($ini." "."a todos");
+            ?>
+    </body>
+</html>
