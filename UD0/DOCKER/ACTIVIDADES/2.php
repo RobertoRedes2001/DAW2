6070b34c82f1467fd919ccbe6eb20deb521d395a
Roberto <romaav@floridauniversitaria.es>
2023-09-19 15:36:52 +0200

    añadidos comentarios al codigo

--- UD0/DOCKER/ACTIVIDADES/2.php
+++ UD0/DOCKER/ACTIVIDADES/2.php
@@ -1,10 +1,12 @@
 <?php
+    //Crea un array de numeros
     $array = array(
         1 => 25,
         2 => 5,
         3 => 62,
     );
 
+    //Imprime el array y luego lo ordena de mayor a menor
     print_r($array);
     echo "<br>";
     rsort($array);
--- UD0/DOCKER/ACTIVIDADES/4.php
+++ UD0/DOCKER/ACTIVIDADES/4.php
@@ -1,30 +1,37 @@
 <?php
 
+    //Eliminamos los espacios sobrantes al frente y detras del string
     $nombre = "  Roberto  ";
     $trimed = trim($nombre, " ");
     echo $trimed;
     echo "<br>";
 
+    //Nos indica la longitud del string
     echo strlen($trimed);
     echo "<br>";
 
+    //Convierte a mayusculas el texto
     echo strtoupper($trimed);
     echo "<br>";
 
+    //Convierte a minusculas el texto
     echo strtolower($trimed);
     echo "<br>";
 
+    //Comprueba si el prefijo indicado se encuentra en el nombre
     $prefijo = "Nombre";
     if(!strpos("Nom",$prefijo)){
         echo "Si empieza.";
     }
     echo "<br>";
 
+    //Cuenta las veces que aparece una letra en la palabra
     echo substr_count($trimed, 'o');
     echo "<br>";
     echo substr_count($trimed, strtoupper('r'));
     echo "<br>";
 
+    //Indica la posicion de la "a" en el string (en caso de haber)
     if(strpos($prefijo,"a")){
         echo strpos($prefijo,"a");
     }else{
