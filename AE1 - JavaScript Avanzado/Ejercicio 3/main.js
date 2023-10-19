8e2653f86b646c7d834d32a8a210ef0182f24203
RobertoRedes2001 <romaav@floridauniversitaria.es>
2023-10-19 11:31:51 +0200

    refactorizado

--- AE1 - JavaScript Avanzado/Ejercicio 3/main.js
+++ AE1 - JavaScript Avanzado/Ejercicio 3/main.js
@@ -2,20 +2,20 @@ function cargarSeccion() {
   document.getElementsByTagName("td")[2].innerHTML = "";
   let container = document.createElement("div");
   container.style.display = "flex";
-  for (i = 0; i < secciones[seccionPosition].contenidos.length; i++) {
+  for (i = 0; i < secciones[seccionPosition].contenido.length; i++) {
     let h1 = document.createElement("h1");
-    h1.style.backgroundImage = "url(" + secciones[seccionPosition].contenidos[i].imagenes[0] + ")"; // Reemplaza "ruta_de_la_imagen.jpg" con la ruta de tu imagen
+    h1.style.backgroundImage = "url(" +secciones[seccionPosition].contenido[i].imagen+ ")"; // Reemplaza "ruta_de_la_imagen.jpg" con la ruta de tu imagen
     h1.style.backgroundSize = "cover"; // Ajusta el tamaño para cubrir completamente el elemento h1
     h1.style.backgroundPosition = "center"; // Centra la imagen en el elemento h1
     h1.style.width = "100%"; // Establece el ancho
     h1.style.height = "300px"; // Establece la altura
     h1.style.color = "white";
     h1.style.textAlign = "center";
-    h1.textContent = secciones[seccionPosition].contenidos[i].nombre;
+    h1.textContent = secciones[seccionPosition].contenido[i].nombre;
     container.appendChild(h1);
   }
   document.getElementsByTagName("td")[2].appendChild(container);
-  document.getElementsByTagName("h1")[0].innerHTML = secciones[seccionPosition].seccion;
+  document.getElementsByTagName("h1")[0].innerHTML = secciones[seccionPosition].nombre;
 }
 
 function siguienteSeccion() {
@@ -38,69 +38,16 @@ function anteriorSeccion() {
 
 let seccionPosition = 0;
 
-let alcohol = [
-  "./src/alcohol/1.jpg",
-  "./src/alcohol/2.jpg",
-  "./src/alcohol/3.jpg",
-  "./src/alcohol/4.jpg",
-  "./src/alcohol/5.jpg",
-];
+let secciones = [
+  {nombre : "BEBIDAS", contenido : [{nombre: "cafe", imagen: './src/cafe/1.jpg'},
+  {nombre: "infusiones", imagen: './src/infusiones/1.jpg'},
+  {nombre: "alcohol", imagen: './src/alcohol/1.jpg'},]},
 
-let cafe = [
-  "./src/cafe/1.jpg",
-  "./src/cafe/2.jpg",
-  "./src/cafe/3.jpg",
-  "./src/cafe/4.jpg",
-  "./src/cafe/5.jpg",
-];
+  {nombre : "POSTRES", contenido : [{nombre: "tartas", imagen: './src/tartas/1.jpg'},
+  {nombre: "fruta", imagen: './src/fruta/1.jpg'},]},
 
-let infusiones = [
-  "./src/infusiones/1.jpg",
-  "./src/infusiones/2.jpg",
-  "./src/infusiones/3.jpg",
-  "./src/infusiones/4.jpg",
-  "./src/infusiones/5.jpg",
-];
-
-let bebidas = {seccion : "BEBIDAS" , contenidos : [
-  { nombre: "cafe", imagenes: cafe },
-  { nombre: "infusiones", imagenes: infusiones },
-  { nombre: "alcohol", imagenes: alcohol },
-]};
-
-let fruta = [
-  "./src/fruta/1.jpg",
-  "./src/fruta/2.jpg",
-  "./src/fruta/3.jpg",
-  "./src/fruta/4.jpg",
-  "./src/fruta/5.jpg",
-];
-
-let tartas = [
-  "./src/tartas/1.jpg",
-  "./src/tartas/2.jpg",
-  "./src/tartas/3.jpg",
-  "./src/tartas/4.jpg",
-  "./src/tartas/5.jpg",
-];
-
-let postres = { seccion: "POSTRES", contenidos : [ 
-  { nombre: "fruta", imagenes: fruta },
-  { nombre: "tartas", imagenes: tartas },
-]};
-
-let entrantes = { seccion: "ENTRANTES", contenidos : [ {
-  nombre: "entrantes",
-  imagenes: [
-    "./src/entrantes/1.jpg",
-    "./src/entrantes/2.jpg",
-    "./src/entrantes/3.jpg",
-    "./src/entrantes/4.jpg",
-    "./src/entrantes/5.jpg",
-  ],}
-]};
-
-let secciones = [bebidas, postres, entrantes];
+  {nombre : "ENTANTES", contenido : [{nombre: "entrantes", imagen: './src/entrantes/1.jpg'},]},
+]
 
 cargarSeccion();
 
