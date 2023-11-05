2e71f5b840f2c4e72fce5b574fad464d0f55ad89
RobertoRedes2001 <romaav@floridauniversitaria.es>
2023-11-05 12:04:33 +0100

    controlador añadido

--- UD2/ACTIVIDADES/MVCsencillo/Public/router.php
+++ UD2/ACTIVIDADES/MVCsencillo/Public/router.php
@@ -1,12 +1,14 @@
 <?php
+require_once('../src/controllers/controler.php');
 require_once('../src/models/dataModel.php');
 require_once('../src/models/listModel.php');
 require_once('../src/views/listView.php');
 
-// Inicializamos el modelo
-$con = new dataModel();
+// Inicializamos el modelo, la vista y el controlador
+$conexion = new dataModel();
 $consulta = new listModel();
 $tabla = new listView();
+$controlador = new controller($consulta,$tabla);
 
 // Obtener la ruta actual desde la URL
 //arreglar la ruta, no coge el valor de la ruta, siempre va al valor false
@@ -17,7 +19,7 @@ echo "<br>";
 switch ($route) {
     case 'list':
         //Conseguimos todos los registros
-        $con->openConection();
+        $conexion->openConection();
         $array = $consulta->getAll();
         $tabla->listTabla($array);
         break;
@@ -25,7 +27,7 @@ switch ($route) {
         //Sacamos el numero del registro si es que existe, si no devolvemos 1
         $number = isset($_GET['number']) ? $_GET['number'] : 1;
         //Conseguimos el registro y lo pasamos por pantalla
-        $con->openConection();
+        $conexion->openConection();
         $array = $consulta->getOne($number);
         $tabla->listTabla($array);
         break;
