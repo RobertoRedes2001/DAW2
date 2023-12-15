39f307e9b6624727a8cedf1badc8a68bf834876f
RobertoRedes2001 <romaav@floridauniversitaria.es>
2023-12-15 11:46:17 +0100

    Avanzado menu

--- SEGUNDA_EV/roberto-app/src/app/app.component.ts
+++ SEGUNDA_EV/roberto-app/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { CabeceraComponent } from './components/cabecera/cabecera.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, CabeceraComponent],
+  imports: [CommonModule, RouterOutlet, CabeceraComponent,MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- SEGUNDA_EV/roberto-app/src/app/components/menu/menu.component.ts
+++ SEGUNDA_EV/roberto-app/src/app/components/menu/menu.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
-
+  public toggle : boolean = false;
+  public toggleList(){
+    this.toggle =! this.toggle;
+  }
 }
