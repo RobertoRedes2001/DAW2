bff5fbd369f8521657d145ce6284c7b41c6830eb
RobertoRedes2001 <romaav@floridauniversitaria.es>
2023-12-30 19:21:45 +0100

    enrutamiento añadido

--- SEGUNDA_EV/roberto-app/src/app/app.component.ts
+++ SEGUNDA_EV/roberto-app/src/app/app.component.ts
@@ -3,12 +3,18 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { HomeComponent } from './views/home/home.component';
+import { LongtermComponent } from './views/longterm/longterm.component';
+import { ShorttermComponent } from './views/shortterm/shortterm.component';
+import { GameComponent } from './views/game/game.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,HeaderComponent,BannerComponent,FooterComponent],
+  imports: [CommonModule, RouterOutlet,HeaderComponent,BannerComponent,
+    HomeComponent, LongtermComponent, ShorttermComponent, GameComponent,
+    FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
