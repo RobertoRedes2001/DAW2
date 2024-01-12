2c10284f8f18c3e155accc4d3fe779d8d009bcc2
RobertoRedes2001 <romaav@floridauniversitaria.es>
2024-01-12 11:44:56 +0100

    iniciada actividad

---
+++ SEGUNDA_EV/roberto-app/src/app/components/card/card.component.ts
@@ -0,0 +1,15 @@
+import { NgStyle } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [NgStyle],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
+})
+export class CardComponent {
+
+  @Input() title : string = 'Fruta';
+  @Input() bg : string = 'https://wikimon.net/images/f/f1/Agumon_re.jpg';
+}
---
+++ SEGUNDA_EV/roberto-app/src/app/views/pescados/pescados.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pescados',
+  standalone: true,
+  imports: [],
+  templateUrl: './pescados.component.html',
+  styleUrl: './pescados.component.css'
+})
+export class PescadosComponent {
+
+}
